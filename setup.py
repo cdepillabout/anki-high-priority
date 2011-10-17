8a5d3a8655b9a1dd4afc3c70424ce62bd4bbad53
(cdep)illabout <cdep.illabout@gmail.com>
2011-10-17 07:01:14 -0400

    Created python packaging.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+
+from distutils.core import setup
+
+setup(name='anki-high-priority',
+      version='1.0',
+      description='add HighPriority tag to existing facts in Anki deck',
+      author='(cdep) illabout',
+      author_email='cdep.illabout@gmail.com',
+      #url='http://www.python.org/sigs/distutils-sig/',
+      scripts=['scripts/anki-high-priority'],
+     )
+
